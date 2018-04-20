61570520ab6048710607f4c070d1a7d3ba40e90e
philipahlberg <philipahlberg@users.noreply.github.com>
2018-04-20 23:26:35 +0200

    improve compile.spec.js

--- test/compile.spec.js
+++ test/compile.spec.js
@@ -1,47 +1,79 @@
 import { compile } from '../dist/index.js';
 
 describe('compile', () => {
-  it('matches a simple path', () => {
-    const pattern = compile('/abc');
-    expect(pattern.test('/abc')).to.be.true;
+  describe('loose', () => {
+    it('matches a simple path', () => {
+      const pattern = compile('/abc');
+      expect(pattern.test('/abc')).to.be.true;
+      expect(pattern.test('/def')).to.be.false;
+      expect(pattern.test('/abc/def')).to.be.true;
+    });
+
+    it('matches parameters', () => {
+      const pattern = compile('/:a/:b/:c');
+      expect(pattern.test('/1/2/3/4')).to.be.true;
+      expect(pattern.test('/1/2/3')).to.be.true;
+      expect(pattern.test('/1/2')).to.be.false;
+      expect(pattern.test('/1')).to.be.false;
+    });
+
+    it('ignores query', () => {
+      const pattern = compile('/abc');
+      expect(pattern.test('/abc?q=123')).to.be.true;
+      expect(pattern.test('/def?q=123')).to.be.false;
+      expect(pattern.test('/abc/def?q=123')).to.be.true;
+    });
+  
+    it('ignores hash', () => {
+      const pattern = compile('/abc');
+      expect(pattern.test('/abc#hash')).to.be.true;
+      expect(pattern.test('/def#hash')).to.be.false;
+      expect(pattern.test('/abc/def#hash')).to.be.true;
+    });
+  
+    it('ignores query and hash', () => {
+      const pattern = compile('/abc');
+      expect(pattern.test('/abc?q=123#hash')).to.be.true;
+      expect(pattern.test('/def?q=123#hash')).to.be.false;
+      expect(pattern.test('/abc/def?q=123#hash')).to.be.true;
+    });
   });
 
-  it('matches loosely', () => {
-    const pattern = compile('/abc');
-    expect(pattern.test('/abc')).to.be.true;
-    expect(pattern.test('/abc/def')).to.be.true;
-  });
+  describe('exact', () => {
+    it('matches a simple path', () => {
+      const pattern = compile('/abc', true);
+      expect(pattern.test('/abc')).to.be.true;
+      expect(pattern.test('/def')).to.be.false;
+      expect(pattern.test('/abc/def')).to.be.false;
+    });
 
-  it('matches exactly', () => {
-    const pattern = compile('/abc', true);
-    expect(pattern.test('/abc')).to.be.true;
-    expect(pattern.test('/abc/def')).to.be.false;
-  });
+    it('matches parameters', () => {
+      const pattern = compile('/:a/:b/:c', true);
+      expect(pattern.test('/1/2/3/4')).to.be.false;
+      expect(pattern.test('/1/2/3')).to.be.true;
+      expect(pattern.test('/1/2')).to.be.false;
+      expect(pattern.test('/1')).to.be.false;
+    });
 
-  it('matches loosely with parameters', () => {
-    const pattern = compile('/:a/:b/:c');
-    expect(pattern.test('/1/2/3')).to.be.true;
-  });
-
-  it('matches exactly with parameters', () => {
-    const pattern = compile('/:a/:b/:c', true);
-    expect(pattern.test('/1/2/3')).to.be.true;
-    expect(pattern.test('/1/2')).to.be.false;
-    expect(pattern.test('/1')).to.be.false;
-  });
-
-  it('ignores query', () => {
-    const pattern = compile('/abc');
-    expect(pattern.test('/abc?q=123')).to.be.true;
-  });
-
-  it('ignores hash', () => {
-    const pattern = compile('/abc');
-    expect(pattern.test('/abc#hash')).to.be.true;
-  });
-
-  it('ignores query and hash', () => {
-    const pattern = compile('/abc');
-    expect(pattern.test('/abc?q=123#hash'));
+    it('does not ignore query', () => {
+      const pattern = compile('/abc', true);
+      expect(pattern.test('/abc?q=123')).to.be.false;
+      expect(pattern.test('/def?q=123')).to.be.false;
+      expect(pattern.test('/abc/def?q=123')).to.be.false;
+    });
+  
+    it('does not ignore hash', () => {
+      const pattern = compile('/abc', true);
+      expect(pattern.test('/abc#hash')).to.be.false;
+      expect(pattern.test('/def#hash')).to.be.false;
+      expect(pattern.test('/abc/def#hash')).to.be.false;
+    });
+  
+    it('does not ignore query and hash', () => {
+      const pattern = compile('/abc', true);
+      expect(pattern.test('/abc?q=123#hash')).to.be.false;
+      expect(pattern.test('/def?q=123#hash')).to.be.false;
+      expect(pattern.test('/abc/def?q=123#hash')).to.be.false;
+    });
   });
 });
