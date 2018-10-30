d581b2db0325e4209634041074ea5246930fd96f
Philip Ahlberg <philipahlberg@users.noreply.github.com>
2018-10-30 21:48:53 +0100

    remove dist

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { parse } from './parse';
-export { compile } from './compile';
-export { execute } from './execute';
-export { map } from './map';
-export { object } from './object';
-export { program } from './program';
--- dist/parse.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Extract the keys in a path declaration.
- * @example
- * parse('/:a/:b/:c'); // => ['a', 'b', 'c']
- *
- * @param path A path declaration
- */
-export declare const parse: (path: string) => string[];
